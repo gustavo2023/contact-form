fb6a02a7be2f4721f32610c2eb0ce7f75174d2d4
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-04-29 21:24:10 -0400

    feat: add functionality to validate that each input is not empty and to handle form submit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const contactForm = document.getElementById("contact-form");
 const firstNameInput = document.getElementById("user-first-name");
 const lastNameInput = document.getElementById("user-last-name");
 const emailInput = document.getElementById("user-email");
+const queryTypeContainer = document.querySelector(".query-type");
 const radioInputs = document.querySelectorAll('input[name="queryType"]');
 const messageInput = document.getElementById("message-input");
 const agreementCheckbox = document.getElementById("agreement-input");
@@ -38,3 +39,65 @@ const hideErrorMessage = (inputElement) => {
     }
   }
 };
+
+const handleFormSubmit = () => {
+  const firstName = firstNameInput.value.trim();
+  const lastName = lastNameInput.value.trim();
+  const email = emailInput.value.trim();
+  const message = messageInput.value.trim();
+  const radioValue = Array.from(radioInputs).find(
+    (radio) => radio.checked
+  )?.value;
+  const agreementChecked = agreementCheckbox.checked;
+
+  if (!firstName) {
+    showErrorMessage(firstNameInput);
+    firstNameInput.classList.add("error-state");
+    return false;
+  }
+
+  if (!lastName) {
+    showErrorMessage(lastNameInput);
+    lastNameInput.classList.add("error-state");
+    return false;
+  }
+
+  if (!validateUserEmail(email) || !email) {
+    showErrorMessage(emailInput);
+    emailInput.classList.add("error-state");
+    return false;
+  }
+
+  if (!message) {
+    showErrorMessage(messageInput);
+    messageInput.classList.add("error-state");
+    return false;
+  }
+
+  if (!radioValue) {
+    showErrorMessage(queryTypeContainer);
+    return false;
+  }
+
+  if (!agreementChecked) {
+    showErrorMessage(agreementCheckbox);
+    return false;
+  }
+
+  return true;
+};
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const isValid = handleFormSubmit();
+
+  if (!isValid) {
+    return;
+  }
+
+  toastContainer.style.display = "flex";
+
+  setTimeout(() => {
+    toastContainer.style.display = "none";
+  }, 5000);
+});
